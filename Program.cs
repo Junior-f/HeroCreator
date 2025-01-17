aa6d603be5516a35ba86e5754b99d58bfade4631
FraanciscoJunior <f.junior@portaltelemedicina.com.br>
2025-01-17 18:03:52 -0300

    feat: add configuration swagger

--- Program.cs
+++ Program.cs
@@ -5,22 +5,20 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configuração do banco de dados
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Configuração de dependências
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.EnableAnnotations();
-});
-
-
+builder.Services.AddSwaggerGen(options => options.EnableAnnotations());
 builder.Services.AddScoped<ICharacterRepository, CharacterRepository>();
 builder.Services.AddScoped<ICharacterService, CharacterService>();
 
 var app = builder.Build();
 
+// Configuração de middlewares
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -30,11 +28,17 @@ if (app.Environment.IsDevelopment())
         c.RoutePrefix = "swagger";
     });
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
-app.UseSwagger();
-app.UseSwaggerUI();
+
+app.MapGet("/", () => "Hello World!")
+   .WithName("HelloWorld");
 
 app.Run();
