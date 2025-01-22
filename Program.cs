2bfdf5808462671e9fef3b97c859ef2557f67402
junior <61066055+Junior-f@users.noreply.github.com>
2025-01-22 13:03:39 -0300

    feat: add MapGet and refactor

--- Program.cs
+++ Program.cs
@@ -5,39 +5,63 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configuração do banco de dados
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+ConfigureDatabase(builder);
 
-// Configuração de dependências
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options => options.EnableAnnotations());
-builder.Services.AddScoped<ICharacterRepository, CharacterRepository>();
-builder.Services.AddScoped<ICharacterService, CharacterService>();
+ConfigureServices(builder);
+
+ConfigureSwagger(builder);
 
 var app = builder.Build();
 
-// Configuração de middlewares
 if (app.Environment.IsDevelopment())
+{
+    ConfigureDevelopment(app);
+}
+
+
+ConfigureMiddleware(app);
+
+app.Run();
+
+void ConfigureDatabase(WebApplicationBuilder builder)
+{
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+}
+
+void ConfigureServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddScoped<ICharacterRepository, CharacterRepository>();
+    builder.Services.AddScoped<ICharacterService, CharacterService>();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+}
+
+void ConfigureSwagger(WebApplicationBuilder builder)
+{
+    builder.Services.AddSwaggerGen(options =>
+    {
+        options.EnableAnnotations();
+    });
+}
+
+void ConfigureDevelopment(WebApplication app)
 {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
-        c.RoutePrefix = "swagger";
     });
+
+    app.UseDeveloperExceptionPage();
 }
-else
+
+void ConfigureMiddleware(WebApplication app)
 {
-    app.UseExceptionHandler("/error");
-    app.UseHsts();
+    app.UseRouting();
+    app.UseHttpsRedirection();
+
+    app.MapGet("/", () => "Hello World!").ExcludeFromDescription();
+
+    app.MapControllers();
 }
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
